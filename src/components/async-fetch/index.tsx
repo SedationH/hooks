cc3679d8a7ef7f70698ce4cc5a3ea4f0d9a06ad8
SedationH <sedationh@gmail.com>
2022-05-24 20:03:01 +0800

    async-fetch

--- src/components/async-fetch/index.tsx
+++ src/components/async-fetch/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { useAsync } from "react-use"
 
 const mockFetch = (id?: number): Promise<string> => {
   return new Promise((resolve) => {
@@ -10,18 +11,14 @@ const mockFetch = (id?: number): Promise<string> => {
 
 export default function AsyncFetch() {
   const [id, setId] = useState(0)
-  const [response, setResponse] = useState<string>("")
-
-  useEffect(() => {
-    mockFetch(id).then(setResponse)
-  }, [id])
+  const { value, loading } = useAsync(() => mockFetch(id), [id])
 
   return (
     <div>
       <div>id : {id}</div>
       <button onClick={() => setId(id + 1)}>addId</button>
       <hr />
-      response: {response}
+      {loading ? <h2>is loading</h2> : <h2>response: {value}</h2>}
     </div>
   )
 }
