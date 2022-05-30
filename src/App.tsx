6cc0881387a3e4aecb06ac42f7e3b83aed7f4534
SedationH <sedationh@gmail.com>
2022-05-30 11:15:22 +0800

    新增路由自动生成能力

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import { useCallback, useMemo, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import AsyncFetch from "./components/async-fetch"
-import SearchParams from "./components/search-params"
+import { Suspense, useCallback, useMemo, useState } from "react"
+import { Link, Route, useRoutes } from "react-router-dom"
 import "./App.css"
-import OptimisticUpdate from "./components/optimistic-update"
-import SWR from "./components/swr"
+import routes from "~react-pages"
 
 function App() {
+  const Routes = useRoutes(routes)
+  console.log("sedationh", routes)
+
   return (
     <div className="App">
       <h1>SedationH's Hooks Playground</h1>
       <nav>
-        <Link to="/search-params">SearchParams</Link>
-        <Link to="/async-fetch">AsyncFetch</Link>
-        <Link to="/optimistic-update">OptimisticUpdate</Link>
-        <Link to="/swr">SWR</Link>
+        {routes.map(({ path = "" }) => (
+          <Link to={path}>{path}</Link>
+        ))}
       </nav>
-      <Routes>
-        <Route path="/search-params" element={<SearchParams />} />
-        <Route path="/async-fetch" element={<AsyncFetch />} />
-        <Route path="/optimistic-update" element={<OptimisticUpdate />} />
-        <Route path="/swr" element={<SWR />} />
-      </Routes>
+      {Routes}
     </div>
   )
 }
